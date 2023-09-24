f65fe3b6299212138deb9f470445fbd287fdff9d
NoekaIzumi <noeka-izumi@outlook.jp>
2023-09-24 17:02:58 +0000

    [updated admin-sign-in]

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,31 @@
 Rails.application.routes.draw do
 
+ root to: "homes#top"
+
+
+get 'items' => 'public/items#index'
+
+ namespace :admin do
+   resources :items
+   get 'items' => 'admin/items#index'
+ end
+
   # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers, skip: [:passwords],controllers: {
   registrations: "public/registrations",
-  sessions: 'public/sessions'
+  sessions: "public/sessions"
 }
 
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords],controllers: {
-  sessions: "admin/sessions"
+  sessions: "admin/sessions",
 }
+
+ get 'home/about' , to: 'homes#about' , as: 'about'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
