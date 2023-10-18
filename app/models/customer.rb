c40425d1de8f37b3db80efc8b5747e5056307b2a
NoekaIzumi <noeka-izumi@outlook.jp>
2023-10-18 15:06:42 +0000

    [Add admin/customer/edit]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum is_deleted: { 有効:false, 無効:true, }
 
   # is_deletedがfalseならtrueを返すようにしている
   def active_for_authentication?
