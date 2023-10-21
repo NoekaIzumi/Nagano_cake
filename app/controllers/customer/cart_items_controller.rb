31f4b1db16a5a83a7b820c63a46af111730f4d61
NoekaIzumi <noeka-izumi@outlook.jp>
2023-10-21 16:23:50 +0000

    [Pause/customer/cart_item/index]

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,2 +1,7 @@
 class Customer::CartItemsController < ApplicationController
+
+  def index
+     @customer = Customer.find(current_customer.id)
+  end
+
 end
--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -2,10 +2,12 @@ class Customer::ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    @customer = Customer.find(current_customer.id)
   end
 
   def show
    @item = Item.find(params[:id])
+   @customer = Customer.find(current_customer.id)
   end
 
 
@@ -14,6 +16,7 @@ private
   def item_params
     params.require(:item).permit(:name, :image, :introduction,:price)
   end
+
 
 
 end
